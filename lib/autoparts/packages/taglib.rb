a8bac5cab4c78875398c6a41af2fdd6e2bb9382d
maximka <maxim.kraev@gmail.com>
2014-10-21 05:35:02 +0000

    taglib fixes

--- lib/autoparts/packages/taglib.rb
+++ lib/autoparts/packages/taglib.rb
@@ -1,18 +1,20 @@
 module Autoparts
   module Packages
-    class TagLib < Package
+    class Taglib < Package
       name 'taglib' # The name of your Box Part (lower case)
       version '1.9.1' # Version of the component that will be  installed.
-      description "TagLib Audio Meta-Data Library"
+      description "TagLib: a library for reading and editing the meta-data of several popular audio formats."
       category Category::LIBRARIES
 
       source_url 'http://taglib.github.io/releases/taglib-1.9.1.tar.gz'
       source_sha1 '4fa426c453297e62c1d1eff64a46e76ed8bebb45'
       source_filetype 'tar.gz'
-      depends_on 'cmake'
+      
+      #compile time dependency
+      #depends_on 'cmake'
       
       def compile
-        Dir.chdir('taglib-1.9.1') do
+        Dir.chdir(name_with_version) do
           args = [
             "-DCMAKE_INSTALL_PREFIX=#{prefix_path}",
             "-DCMAKE_RELEASE_TYPE=Release",
@@ -24,7 +26,7 @@ module Autoparts
       end
 
       def install
-        Dir.chdir('taglib-1.9.1') do
+        Dir.chdir(name_with_version) do
           execute 'make install'
         end
       end    
