d0915ea5a56111bf1b5b17a4ec525013bc148108
maximka <maxim.kraev@gmail.com>
2014-10-23 10:13:50 +0000

    with python flasg added to gdal

--- lib/autoparts/packages/gdal.rb
+++ lib/autoparts/packages/gdal.rb
@@ -1,22 +1,21 @@
-# Copyright (c) 2013-2014 Irrational Industries Inc. d.b.a. Nitrous.IO
-# This software is licensed under the [BSD 2-Clause license](https://raw.github.com/nitrous-io/autoparts/master/LICENSE).
-
-# All python packages should be rebuild on version change.
 module Autoparts
   module Packages
     class Gdal < Package
       name 'gdal'
-      version '1.11.1'
+      version '1.11.1-1'
       description 'GDAL: a translator library for raster and vector geospatial data formats.'
       source_url 'http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz'
       source_sha1 'e2c67481932ec9fb6ec3c0faadc004f715c4eef4'
       source_filetype 'tar.gz'
       category Category::LIBRARIES
       
+#      depends_on "python2"
+      
       def compile
-        Dir.chdir(name_with_version) do
+        Dir.chdir("gdal-1.11.1") do
           args = [
-            "--prefix=#{prefix_path}"
+            "--prefix=#{prefix_path}",
+            "--with-python"
           ]          
           execute './configure', *args
           execute 'make'
@@ -24,7 +23,7 @@ module Autoparts
       end
       
       def install
-        Dir.chdir(name_with_version) do
+        Dir.chdir("gdal-1.11.1") do
           execute 'make install'
         end
       end
