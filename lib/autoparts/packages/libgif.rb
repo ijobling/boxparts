d00866dbd65c2c9e48824bfa59bba078b011fbe0
d4mation <eric.defore@gmail.com>
2015-02-13 20:48:34 +0000

    npm install gameboy fails because it is missing dependencies. These Box Parts add those dependencies to the system. libcairo2 depends on pixman and libgif is a standard part of many Linux Distros.

---
+++ lib/autoparts/packages/libgif.rb
@@ -0,0 +1,37 @@
+module Autoparts
+  module Packages
+    class Libgif < Package
+      name 'libgif'
+      version '4.1.6'
+      description 'GIFLIB: package of portable tools and library routines for working with GIF images'
+      category Category::LIBRARIES
+
+      source_url 'http://ftp.de.debian.org/debian/pool/main/g/giflib/giflib_4.1.6.orig.tar.gz'
+      source_sha1 '8649fa9c16f3300750f7089c79e652ddc750c832'
+      source_filetype 'tar.gz'
+
+      def name_with_version
+        "giflib-#{version}"
+      end
+
+      def compile
+        Dir.chdir(name_with_version) do
+          args = [
+            "--prefix=#{prefix_path}",
+            ]
+          execute './configure', *args
+          execute 'make'
+        end
+      end
+      
+      def install
+        Dir.chdir(name_with_version) do
+          execute 'make install'
+        end
+      end
+      
+      
+      
+    end
+  end
+end
---
+++ lib/autoparts/packages/pixman.rb
@@ -0,0 +1,38 @@
+module Autoparts
+  module Packages
+    class Pixman < Package
+      name 'pixman'
+      version '0.32.6'
+      description 'pixman: low-level software library for pixel manipulation, providing features such as image compositing and trapezoid rasterization'
+      category Category::LIBRARIES
+
+      source_url 'http://cairographics.org/releases/pixman-0.32.6.tar.gz'
+      source_sha1 '8791343cbf6d99451f4d08e8209d6ac11bf96df2'
+      source_filetype 'tar.gz'
+
+      def name_with_version
+        "pixman-#{version}"
+      end
+
+      def compile
+        Dir.chdir(name_with_version) do
+          
+          args = [
+            "--prefix=#{prefix_path}",
+            ]
+          execute './configure', *args
+          execute 'make'
+        end
+      end
+      
+      def install
+        Dir.chdir(name_with_version) do
+          execute 'make install'
+        end
+      end
+      
+      
+      
+    end
+  end
+end
