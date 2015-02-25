306fba14a3371942c66484be6e4c57f99e0a02db
ianjobling <ijobling@codio.com>
2015-02-25 14:17:40 +0000

    update openldap

--- lib/autoparts/packages/openldap.rb
+++ lib/autoparts/packages/openldap.rb
@@ -2,10 +2,10 @@ module Autoparts
   module Packages
     class Openldap < Package
       name 'openldap'
-      version '2.4.39'
+      version '2.4.40'
       description 'OpenLDAP:  an open source implementation of the Lightweight Directory Access Protocol.'
-      source_url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.39.tgz'
-      source_sha1 '2b8e8401214867c361f7212e7058f95118b5bd6c'
+      source_url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.40.tgz'
+      source_sha1 '0cfac3b024b99de2e2456cc7254481b6644e0b96'
       source_filetype 'tgz'
       category Category::UTILITIES
 
