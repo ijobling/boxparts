8ab8140ef7968c94cede505ce23de9d93553a544
jamesjnadeau <james.nadeau@gmail.com>
2015-06-02 12:11:32 +0000

    bumping base googleappengine version to 1.9.21

--- lib/autoparts/packages/googleappengine.rb
+++ lib/autoparts/packages/googleappengine.rb
@@ -5,12 +5,12 @@ module Autoparts
   module Packages
     class GoogleAppEngine < Package
       name 'googleappengine'
-      version '1.9.17'
+      version '1.9.21'
       description 'Google App Engine for Python/PHP: A CLI for managing Google App Engine cloud services for Python and PHP'
       category Category::DEPLOYMENT
 
-      source_url 'https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.17.zip'
-      source_sha1 'eec50aaf922d3b21623fda1b90e199c3ffa9e16e'
+      source_url 'https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.21.zip'
+      source_sha1 'ad616c194337614888aad97c38792507dc706413'
       source_filetype 'zip'
 
       def install
