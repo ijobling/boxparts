dd715b086768875d3631085403f16112d83ce5a6
ianjobling <ijobling@codio.com>
2015-07-24 10:30:54 +0000

    update dart 1.11.1

--- lib/autoparts/packages/dart.rb
+++ lib/autoparts/packages/dart.rb
@@ -2,10 +2,10 @@ module Autoparts
   module Packages
     class Dart < Package
       name 'dart'
-      version '1.8.3'
+      version '1.11.1'
       description 'Dart is a new platform for scalable web app engineering'
-      source_url 'https://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-x64-release.zip'
-      source_sha1 '562c1101b702ead23e24f1652a5b7342935b2abd'
+      source_url 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.11.1/sdk/dartsdk-linux-x64-release.zip'
+      source_sha1 '91c5102df157c09d15ba0ffa415823a9ae99c0da'
       source_filetype 'zip'
       category Category::PROGRAMMING_LANGUAGES
 
